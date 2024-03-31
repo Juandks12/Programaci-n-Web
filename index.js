1eb507d6586071a82778553a151ade0af08b1910
Juan Peña <juandk1016@hotmail.com>
2024-03-31 11:55:13 -0500

    update 1

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const d = document
+ const $themeBtn = d.querySelector(".dark-theme-btn");
+ const $selectors = d.querySelectorAll("[data-dark]") // ESLA DATA QUE SE CREO  y al poner los corchetes 
+                                                      // Es como decira que todos los elemetnos que tengan ese query
+//console.log($themeBtn.textContent)
+const classDark = "dark-mode";
+ const sun = "☀️";
+ const moon = "🌙";
+ const lightMode = () => {
+        $selectors.forEach(el => el.classList.remove(classDark));
+        $themeBtn.textContent = moon
+  };
+ const darkMode = () => {
+        $selectors.forEach(el => el.classList.add(classDark));
+        $themeBtn.textContent = sun
+  };
 
-d.querySelector("#boton").addEventListener("click", (e) => d.querySelectorAll("h1 h2 h3 h4 h5 h6").style.color = "blue")
+
+  d.addEventListener("click", (e) =>{
+
+  if(e.target.matches(".dark-theme-btn")){
+    if($themeBtn.textContent === moon){
+      darkMode()
+    }else{
+      lightMode()
+    }
+  }
+  })
+
+  
